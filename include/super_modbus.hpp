dd56f111128f316855c6d64970d30b694200c01c
Erik Schumacher <erik.d.schumacher@gmail.com>
2024-06-11 04:21:58 +0000

    hello world code + initial cmakelists setup

---
+++ include/super_modbus.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+inline void super_modbus_hello() { std::cout << "Hello from super_modbus!\n"; }
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "super_modbus.hpp"
+
+int main() {
+  std::cout << "Hello from main!\n";
+  super_modbus_hello();
+  return 0;
+}

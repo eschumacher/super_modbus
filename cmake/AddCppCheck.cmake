c22c1cd408d5384951caa0d02540124276a06691
Erik Schumacher <erik.d.schumacher@gmail.com>
2026-02-01 05:25:44 +0000

    Resolved incorrect clang-14 apt dependency; cppcheck; doc output path

--- cmake/AddCppCheck.cmake
+++ cmake/AddCppCheck.cmake
@@ -1,7 +1,12 @@
 function(AddCppCheck target)
-  find_program(CPPCHECK_PATH cppcheck REQUIRED)
-  set_target_properties(${target}
-    PROPERTIES CXX_CPPCHECK
-    "${CPPCHECK_PATH};--enable=warning;--error-exitcode=10;--library=googletest"
-  )
+  find_program(CPPCHECK_PATH cppcheck)
+  if(CPPCHECK_PATH)
+    set_target_properties(${target}
+      PROPERTIES CXX_CPPCHECK
+      "${CPPCHECK_PATH};--enable=warning;--error-exitcode=10;--library=googletest"
+    )
+    message(STATUS "cppcheck found: ${CPPCHECK_PATH}")
+  else()
+    message(STATUS "cppcheck not found, skipping static analysis")
+  endif()
 endfunction()
